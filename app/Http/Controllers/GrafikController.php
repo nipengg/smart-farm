934c903d4af9702b460a11627b35d4360a67689f
MoloKrakein <1123atha@gmail.com>
2022-11-08 22:16:06 +0700

    Route Data Table + Grafik

---
+++ app/Http/Controllers/GrafikController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use App\Models\Pool;
+// use App\Models\PoolData;
+use Illuminate\Http\Request;
+
+class GrafikController extends Controller
+{
+
+    public function grafik()
+    {
+
+
+
+        // $kolam = Pool::all();
+
+        // if ($from == NULL && $to == NULL) {
+        //     $ph = PoolData::select('ph_val', 'created_at')->where("pool_id", $id)->latest()->limit(20)->get();
+        //     $oxygen = PoolData::select('oxygen_val', 'created_at')->where("pool_id", $id)->latest()->limit(20)->get();
+        //     $humidity = PoolData::select('humidity_val', 'created_at')->where("pool_id", $id)->latest()->latest()->limit(20)->get();
+        //     $TDS = PoolData::select('tds_val', 'created_at')->where("pool_id", $id)->latest()->limit(20)->get();
+        //     $temperature = PoolData::select('temper_val', 'created_at')->where("pool_id", $id)->latest()->limit(20)->get();
+        //     $turbidity = PoolData::select('turbidities_val', 'created_at')->where("pool_id", $id)->latest()->limit(20)->get();
+        // } else {
+        //     $ph = PoolData::select('ph_val', 'created_at')->where("pool_id", $id)->latest()->whereBetween("created_at", [$from, $to])->orderBy('created_at', 'desc')->get();
+        //     $oxygen = PoolData::select('oxygen_val', 'created_at')->where("pool_id", $id)->latest()->whereBetween("created_at", [$from, $to])->orderBy('created_at', 'desc')->get();
+        //     $humidity = PoolData::select('humidity_val', 'created_at')->where("pool_id", $id)->latest()->whereBetween("created_at", [$from, $to])->orderBy('created_at', 'desc')->get();
+        //     $TDS = PoolData::select('tds_val', 'created_at')->where("pool_id", $id)->latest()->whereBetween("created_at", [$from, $to])->orderBy('created_at', 'desc')->get();
+        //     $temperature = PoolData::select('temper_val', 'created_at')->where("pool_id", $id)->latest()->whereBetween("created_at", [$from, $to])->orderBy('created_at', 'desc')->get();
+        //     $turbidity = PoolData::select('turbidities_val', 'created_at')->where("pool_id", $id)->latest()->whereBetween("created_at", [$from, $to])->orderBy('created_at', 'desc')->get();
+        // }
+
+        return view('dataSensor.datagrafik');
+    }
+
+    public function table(Request $request)
+    {
+        // {
+        // $id = $request->pool;
+        // $from = $request->from;
+        // $to = $request->to;
+
+        // if ($id == NULL) {
+        //     $pool = Pool::all();
+        //     if ($pool->isEmpty()) {
+        //         $id = null;
+        //     } else {
+        //         $first = $pool->first();
+        //         $id = $first->id;
+        //     }
+        // }
+
+        // if ($from == NULL && $to == NULL) {
+        //     $data = PoolData::where("pool_id", $id)->orderBy('created_at', 'desc')->get();
+        // } else {
+        //     $data = PoolData::where("pool_id", $id)->whereBetween("created_at", [$from, $to])->orderBy('created_at', 'desc')->get();
+        // }
+
+        // $count = $data->count();
+        // if ($count == NULL) {
+        //     $count = 1;
+        // }
+
+        // $kolam = Pool::all();
+
+        return view('dataSensor.datatable');
+    }
+}
--- resources/views/layout/topsideBar.blade.php
+++ resources/views/layout/topsideBar.blade.php
@@ -116,13 +116,13 @@
                                 </a>
                                 <ul class="nav nav-treeview">
                                     <li class="nav-item">
-                                        <a href="#" class="nav-link">
+                                        <a href="/datasensor/table" class="nav-link">
                                             <i class="far fa-circle nav-icon"></i>
                                             <p>Tabel</p>
                                         </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a href="#" class="nav-link">
+                                        <a href="/datasensor/grafik" class="nav-link">
                                             <i class="far fa-circle nav-icon"></i>
                                             <p>Grafik</p>
                                         </a>
