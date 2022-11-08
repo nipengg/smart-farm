de2f31ccf7998e54fd86c8bb9ce1e4b7173cd3e3
MoloKrakein <1123atha@gmail.com>
2022-11-08 20:57:57 +0700

    Tabel + route

---
+++ app/Http/Controllers/LahanController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lahan;
+use Illuminate\Http\Request;
+
+class LahanController extends Controller
+{
+    public function index()
+    {
+        $Lahans = Lahan::all();
+
+        return view('lahan.index', ['Lahans' => $Lahans]);
+    }
+
+    public function create()
+    {
+        return view('lahan.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        Lahan::create($data);
+        return redirect()->route('lahan');
+    }
+
+    public function edit($id)
+    {
+        $Lahan = Lahan::findOrFail($id);
+
+        return view('lahan.edit', ['Lahan' => $Lahan]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $Lahan = Lahan::findOrFail($id);
+        $Lahan->update($data);
+
+        return redirect()->route('lahan');
+    }
+
+    public function destroy($id)
+    {
+        $data = Lahan::findOrFail($id);
+        $data->delete();
+
+        return redirect()->route('lahan');
+    }
+}
