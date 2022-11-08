bb2d65f9c6a6c1b1dc0b6e93f52df28fbd9dc662
Rifaldi Indrajaya <rifaldibpn2@gmail.com>
2022-11-08 21:11:14 +0700

    dashboard

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,37 +1,33 @@
 @extends('layout.topsideBar')
 
 @section('content')
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>Dashboard</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="/">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard</li>
-                    </ol>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-            <!-- Info boxes -->
-            <div class="row">
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Dashboard</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Dashboard v2</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<section class="content">
+    <div class="container-fluid">
+        <!-- Info boxes -->
+        <div class="row">
                 <div class="col-12 col-sm-6 col-md-4">
                     <div class="info-box">
                         <span class="info-box-icon bg-info elevation-1"><i class="fas fa-tint"></i></span>
 
                         <div class="info-box-content">
-                            <span class="info-box-text">Kolam</span>
+                            <span class="info-box-text">Lahan</span>
                             <span class="info-box-number">
-                                <small>Kolam</small>
+                                <small>Lahan</small>
                             </span>
                         </div>
                         <!-- /.info-box-content -->
@@ -45,7 +41,7 @@
 
                         <div class="info-box-content">
                             <span class="info-box-text">Jumlah Data Kolam</span>
-                            <span class="info-box-number"></span>
+                            <span class="info-box-number">10</span>
                         </div>
                         <!-- /.info-box-content -->
                     </div>
@@ -63,7 +59,7 @@
 
                         <div class="info-box-content">
                             <span class="info-box-text">User Count</span>
-                            <span class="info-box-number"></span>
+                            <span class="info-box-number">10</span>
                         </div>
                         <!-- /.info-box-content -->
                     </div>
@@ -71,17 +67,113 @@
                 </div>
                 <!-- /.col -->
             </div>
-            <!-- /.row -->
-       
+        <!-- /.row -->
 
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title">Lahan 1</h5>
 
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                            
+                            <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <p class="text-center">
+                                    <strong>Sales: 1 Jan, 2014 - 30 Jul, 2014</strong>
+                                </p>
 
-            <!-- Main Footer -->
-            <footer class="main-footer">
-                <strong>Copyright &copy; 2022 <a href="#">Aquaponic</a>.</strong>
-                All rights reserved.
-            </footer>
+                                <div class="chart">
+                                    <!-- Sales Chart Canvas -->
+                                    <canvas id="salesChart" height="180" style="height: 180px;"></canvas>
+                                </div>
+                                <!-- /.chart-responsive -->
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-md-4">
+                                <p class="text-center">
+                                    <strong>Last Value</strong>
+                                </p>
+
+                                <div class="progress-group" style="padding-bottom: 20px;">
+                                    PH
+                                    <span class="float-right"><b>160</b>/200</span>
+                                    <div class="progress progress-sm">
+                                        <div class="progress-bar bg-primary" style="width: 80%"></div>
+                                    </div>
+                                </div>
+                                <!-- /.progress-group -->
+                                
+                                <div class="progress-group"style="padding-bottom: 20px;">
+                                    Kelembapan Tanah
+                                    <span class="float-right"><b>310</b>/400</span>
+                                    <div class="progress progress-sm">
+                                        <div class="progress-bar bg-danger" style="width: 75%"></div>
+                                    </div>
+                                </div>
+                                <div class="progress-group"style="padding-bottom: 20px;">
+                                    Aquator
+                                    <span class="badge badge-success">On</span>
+                                </div>
+                                <!-- /.progress-group -->
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                    <!-- ./card-body -->
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-sm-6 col-12">
+                                <div class="description-block border-right">
+                                    <span class="description-percentage text-success"><i class="fas fa-caret-up"></i> 17%</span>
+                                    <h5 class="description-header">$35,210.43</h5>
+                                    <span class="description-text">TOTAL REVENUE</span>
+                                </div>
+                                <!-- /.description-block -->
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-sm-6 col-12">
+                                <div class="description-block border-right">
+                                    <span class="description-percentage text-warning"><i class="fas fa-caret-left"></i> 0%</span>
+                                    <h5 class="description-header">$10,390.90</h5>
+                                    <span class="description-text">TOTAL COST</span>
+                                </div>
+                                <!-- /.description-block -->
+                            </div>
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                    <!-- /.card-footer -->
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
         </div>
-        <!-- ./wrapper -->
+    </div>
+    <!--/. container-fluid -->
+</section>
 
-    @endsection
+
+
+
+
+<!-- Main Footer -->
+<footer class="main-footer">
+    <strong>Copyright &copy; 2022 <a href="#">Aquaponic</a>.</strong>
+    All rights reserved.
+</footer>
+</div>
+<!-- ./wrapper -->
+
+@endsection
