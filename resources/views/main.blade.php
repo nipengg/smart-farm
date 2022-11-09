4702bdf2fa44741b0afb84727b49d1fc3b376439
MoloKrakein <1123atha@gmail.com>
2022-11-09 21:29:19 +0700

    Minor UI update

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,7 +10,7 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard v2</li>
+                        <li class="breadcrumb-item active">Dashboard</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
@@ -22,7 +22,7 @@
             <div class="row">
                 <div class="col-12 col-sm-6 col-md-4">
                     <div class="info-box">
-                        <span class="info-box-icon bg-info elevation-1"><i class="fa-solid fa-seedling"></i></span>
+                        <span class="info-box-icon bg-success elevation-1"><i class="fa-solid fa-seedling"></i></span>
 
                         <div class="info-box-content">
                             <span class="info-box-text">Lahan</span>
@@ -37,7 +37,7 @@
                 <!-- /.col -->
                 <div class="col-12 col-sm-6 col-md-4">
                     <div class="info-box mb-3">
-                        <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thermometer"></i></span>
+                        <span class="info-box-icon bg-olive elevation-1"><i class="fa-solid fa-border-all"></i></span>
 
                         <div class="info-box-content">
                             <span class="info-box-text">Jumlah Data Lahan</span>
