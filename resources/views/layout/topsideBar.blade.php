89808b0b50242eaed6e3724bc27d3d51723c9d7a
Hadad Al Akbar <akbarhadadal@gmail.com>
2022-12-10 12:22:09 +0700

    sidebar bug fix

--- resources/views/layout/topsideBar.blade.php
+++ resources/views/layout/topsideBar.blade.php
@@ -85,9 +85,9 @@
         </nav>
 
         <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-style">
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="{{ url('/') }}" class="brand-link">
+            <a href="{{ url('/') }}" class="brand-link" style="background-color: #0000;">
                 <img src="{{ asset('AdminLTE/dist/img/smart-farm-logo.png') }}" alt="smart-farm-logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light ml-3">SMART FARM</span>
